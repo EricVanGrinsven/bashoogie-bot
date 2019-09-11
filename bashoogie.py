92af6c48a654a48a7d765cea32645e6322f196bd
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-11 14:39:52 -0700

    testing sent from script

--- bashoogie.py
+++ bashoogie.py
@@ -60,7 +60,7 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
     
-send_message(get_last_chat_id_and_text(updates);
+send_message(get_last_chat_id_and_text(updates)
 
 #def main():
     
