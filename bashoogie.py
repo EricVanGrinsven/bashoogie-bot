5c474b5dd866e760ed4dd3fb6d7bc116343c6b6f
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-12 11:36:41 -0700

    testing sent from script

--- bashoogie.py
+++ bashoogie.py
@@ -1,75 +1,37 @@
-import json 
-import requests
-import time
+  
 import os
-from telegram.ext import Updater
-TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
+
+from flask import Flask, request
+
+import telebot
+
+TOKEN = '771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
 
 
-def get_updates(offset=None):
-    url = URL + "getUpdates?timeout=100"
-    if offset:
-        url += "&offset={}".format(offset)
-    js = get_json_from_url(url)
-    return js
-
-def echo_all(updates):
-    for update in updates["result"]:
-        try:
-            text = update["message"]["text"]
-            chat = update["message"]["chat"]["id"]
-            send_message(text, chat)
-        except Exception as e:
-            print(e)
-            
-def get_last_update_id(updates):
-    update_ids = []
-    for update in updates["result"]:
-        update_ids.append(int(update["update_id"]))
-    return max(update_ids)
-    
-def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
-def send_message(text, chat_id):
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
-    
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://young-waters-97525.herokuapp.com/' + TOKEN)
+    return "!", 200
 
 
-def main():
-    updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-    updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
-    #updater.idle()
-    last_update_id = None
-    updates = get_updates(last_update_id)
-    if len(updates["result"]) > 0:
-            last_update_id = get_last_update_id(updates) + 1
-            echo_all(updates)
-    
-if __name__ == '__main__':
-    main()
-
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
