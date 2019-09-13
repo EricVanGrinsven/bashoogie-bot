9c797dd9828c434683764cd6ac4514068eb65711
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 16:19:31 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -12,7 +12,11 @@ def echo(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 # add handlers
 echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
-updater.idle()
+dispatcher.add_handler(echo_handler)    
+def main()
+    updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
+    updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
