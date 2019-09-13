7b1efd710733f2351c2dc15305d1aa730edcc456
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 16:00:16 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -2,12 +2,12 @@ import telegram
 import os
 
 bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
-print(bot.get_me())
-
+#print(bot.get_me())
 TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
 # add handlers
 updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
-updater.idle()
+
+#updater.idle()
