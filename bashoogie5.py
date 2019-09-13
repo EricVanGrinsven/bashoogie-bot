fe15150f95d7aedaf55a884c64dd824c05a3c317
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 16:46:50 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -8,13 +8,13 @@ TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
-def echo(update, context):
+def talk(update, context):
     message2 = update.message.text
     if (message2.in("hey"))
-        context.bot.send_message(chat_id=update.message.chat_id, text=")
+        context.bot.send_message(chat_id=update.message.chat_id, text="fuck u")
 # add handlers
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)    
+talk_handler = MessageHandler(Filters.text, talk)
+dispatcher.add_handler(talk_handler)    
 def main()
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
