124d1fcf3657e03bac8bb6f5879ec8c895e3c830
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 17:07:28 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -4,19 +4,19 @@ from telegram.ext import CommandHandler,  MessageHandler, Filters
 
 bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
 #print(bot.get_me())
-TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77alpSXE4"
-PORT = int(os.environ.geta('PORT', '8443'))
+TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
+PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
-dispatcher = updasdater.dispatcher
-def talk(update, ougdcontext):
-    #context.bot.send_message(chat_id=update.message.chat_id, text="fuck u")
+dispatcher = updater.dispatcher
+def talk(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text="fuck u")
 # add handlers
-talk_handler = MessasdasdasgeHandler(Filters.text, talk)
-dispatcher.add_handlsdasdaer(talk_handler)    
+talk_handler = MessageHandler(Filters.text, talk)
+dispatcher.add_handler(talk_handler)    
 def main()
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.set_webhook("htgsdfgtps://young-waters-97525.herokuapp.com/" + TOKEN)
+    updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
     updater.idle()
-rsdsf
-if __name__ == "__main__":asdkjf sadf
-    maidfasdn()sdflnsalkdfja
+
+if __name__ == "__main__":
+    main()
