b091d2524494560eded7a1b81150bd97a0429881
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-14 12:48:32 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -1,7 +1,7 @@
 import telegram
 import os
 from telegram.ext import Updater
-
+from telegram.ext import MessageHandler, Filters, CommandHandler
 TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN, use_context=True)
@@ -14,9 +14,12 @@ dispatcher = updater.dispatcher
 #dispatcher.add_handler(echo_handler)    
 def echo(update, context):
     message2 = update.message.text
+    user2 = update.message.from_user
+  context.bot.send_message(chat_id=update.message.chat_id, text=message2)
+    
     if "fuck" in  message2:
         context.bot.send_message(chat_id=update.message.chat_id, text=message2)
-from telegram.ext import MessageHandler, Filters, CommandHandler
+
 echo_handler = MessageHandler(Filters.text, echo)
 dispatcher.add_handler(echo_handler)
 bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
@@ -26,5 +29,5 @@ def main():
     updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
     updater.idle()
 
-if __name__ == "__main__":
+if name == "main":
     main()
