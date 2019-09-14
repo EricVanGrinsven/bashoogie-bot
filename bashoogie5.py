417fd3e001609d6a15bf5987d2fb8f0ccb2ee117
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 19:14:39 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -1,7 +1,12 @@
 import telegram
 import os
 from telegram.ext import Updater
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
+from telegram.ext import MessageHandler, Filters
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
 print(bot.get_me())
 TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
