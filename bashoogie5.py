a8411020777d8444cd8f66f3380ee583b9eb7e45
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 19:19:30 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -1,12 +1,7 @@
 import telegram
 import os
 from telegram.ext import Updater
-def echo(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-from telegram.ext import MessageHandler, Filters, CommandHandler
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
-bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
+
 print(bot.get_me())
 TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
 PORT = int(os.environ.get('PORT', '8443'))
@@ -18,6 +13,14 @@ dispatcher = updater.dispatcher
 # add handlers
 #echo_handler = MessageHandler(Filters.text, echo)
 #dispatcher.add_handler(echo_handler)    
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+from telegram.ext import MessageHandler, Filters, CommandHandler
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
+
 def main():
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
