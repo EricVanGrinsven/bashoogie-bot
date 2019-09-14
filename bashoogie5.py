1dfed80665b7ce8c14d423fd373119bac4f35e19
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-13 18:30:27 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -3,16 +3,16 @@ import os
 from telegram.ext import Updater, CommandHandler,  MessageHandler, Filters
 
 bot = telegram.Bot(token='771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4')
-#print(bot.get_me())
+print(bot.get_me())
 TOKEN = "771496641:AAFxDXFGa67rTkzJcnYo0BjDlwI77lpSXE4"
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
-def echo(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+#def echo(update, context):
+ #   context.bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 # add handlers
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)    
+#echo_handler = MessageHandler(Filters.text, echo)
+#dispatcher.add_handler(echo_handler)    
 def main():
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
     updater.bot.set_webhook("https://young-waters-97525.herokuapp.com/" + TOKEN)
