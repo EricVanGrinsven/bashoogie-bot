e7056d958f0afd3633c981dd3df73e82cca21e8c
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-14 21:15:21 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -24,8 +24,8 @@ def echo(update, context):
     counter = 0
     for link in content.find_all('a'):
         counter+=1
-        if counter == 20:
-            context.bot.send_message(chat_id=receivedMessage.chat_id, text=link.get('href'))
+        if counter == 21:
+            context.bot.send_message(chat_id=receivedMessage.chat_id, text= 'youtube.com' + link.get('href'))
     #if "fuck" in message2:
     #    context.bot.send_message(chat_id=update.message.chat_id, text=message2)
 echo_handler = MessageHandler(Filters.text, echo)
