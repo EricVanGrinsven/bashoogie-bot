bd89f93aae4f05d6680d972b394f56ebf6dbf077
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-15 12:09:00 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -25,9 +25,9 @@ def echo(update, context):
     set = True
     for link in content.find_all('a'):
         counter+=1
-        if counter >= 47:
+        if counter >= 47 and "google" not in link.get('href') and set == True:
             context.bot.send_message(chat_id=receivedMessage.chat_id, text= 'youtube.com' + link.get('href'))
-            set = False
+            set = alse
     #if "fuck" in message2:
     #    context.bot.send_message(chat_id=update.message.chat_id, text=message2)
 echo_handler = MessageHandler(Filters.text, echo)
