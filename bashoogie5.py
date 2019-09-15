85057b2b582441e913daa9b69d63c3b20e56f40a
Eric Van Grinsven <evangrin@ucsd.edu>
2019-09-15 12:41:46 -0700

    testing sent from script

--- bashoogie5.py
+++ bashoogie5.py
@@ -17,8 +17,8 @@ def echo(update, context):
     message2 = receivedMessage.text
     user2 = receivedMessage.from_user
     if "@Jeffy" in message2 or "@BashoogieBot" in message2:
-        message2.replace('@Jeffy', '')
-        message2.replace('@BashoogieBot', '')
+        message2 = message2.replace('@Jeffy', '')
+        message2 = message2.replace('@BashoogieBot', '')
         #context.bot.send_message(chat_id=receivedMessage.chat_id, text=receivedMessage.message_id)
         message2 = message2.replace(' ', '+')
         url = 'https://www.youtube.com/results?search_query='
