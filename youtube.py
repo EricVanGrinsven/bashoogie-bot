7069b073bf03aa621acf7b3b979a65a451fee3b1
Eric Van Grinsven <evangrin@ucsd.edu>
2020-08-20 14:16:48 -0700

    revert

--- youtube.py
+++ youtube.py
@@ -24,11 +24,9 @@ def video(receivedMessage, message2, context):
     keywords = ["google", "user", "channel"] # Words that we do NOT wantto link
     keywordInLine = False
     notLinked = True # Used to only link 1 video
-    context.bot.send_message(chat_id=receivedMessage.chat_id, text='initial 1 test for eric')
     # Loop through all 'a' lines that have an 'href' link to search for top resulted video
     for link in content.find_all('a'):
         counter+=1
-        context.bot.send_message(chat_id=receivedMessage.chat_id, text='test for eric')
         for i in keywords:
             if i in link.get('href'):
                 keywordInLine = True
